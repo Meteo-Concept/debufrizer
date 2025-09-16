0e5f0a76a2802472f6e0bdcc581cbbea137d69fb
lgeorget <laurent.georget@meteo-concept.fr>
2025-09-16 12:05:29 +0200

    Have the program send most output to stderr and only output the date of the output file on stdout

--- main.cpp
+++ main.cpp
@@ -9,25 +9,21 @@
 int main(int argc, char** argv)
 {
 	if (argc <= 1 || argc > 3) {
-		std::println(std::cerr, "Usage: {} <BUFR Météo France file> [<output file>]", argv[0]);
+		std::println(std::cerr, "Usage: {} <BUFR Météo France file> [<output file prefix>]", argv[0]);
 		return 1;
 	}
 	std::string inputFile = std::string{argv[1]};
 	std::ifstream is{inputFile, std::ios::binary};
 
-	std::cerr << std::boolalpha << "Parser is ready: " << bool(is) << std::endl;
-
 	Section0 section0;
 	is >> section0;
 
-	std::cout << section0 << std::endl;
-	std::cerr << std::boolalpha << "Parsing is going alright after section 0: " << bool(is) << std::endl;
+	std::cerr << section0 << std::endl;
 
 	Section1 section1;
 	is >> section1;
 
-	std::cout << section1 << std::endl;
-	std::cerr << std::boolalpha << "Parsing is going alright after section 1: " << bool(is) << std::endl;
+	std::cerr << section1 << std::endl;
 
 	if (!section1.hasSection2()) {
 		std::cerr << "No section 2" << std::endl;
@@ -39,12 +35,13 @@ int main(int argc, char** argv)
 	);
 	is >> section3And4;
 
-	std::cout << section3And4 << std::endl;
-	std::cerr << std::boolalpha << "Parsing is going alright after section 3: " << bool(is) << std::endl;
+	std::cerr << section3And4 << std::endl;
 
-	std::string outputFile = inputFile + ".tif";
+	std::string outputFile = std::format("{0:%Y}-{0:%m}-{0:%d}_{0:%H}-{0:%M}-{0:%S}.tif", section1.getTime());
 	if (argc == 3) {
-		outputFile = argv[2];
+		outputFile = argv[2] + outputFile;
 	}
 	section3And4.buildTiff(outputFile);
+
+	std::cout << outputFile << std::endl;
 }
